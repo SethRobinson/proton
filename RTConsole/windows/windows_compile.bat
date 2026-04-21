50dbb049465a6ea120264c94243e8807a804b58c
m6o <skidmuo@gmail.com>
2026-04-21 23:39:47 +0300

    tweaks to cmakelists and the windows_compile.bat command - specifically enable CMAKE_EXPORT_COMPILE_COMMANDS and use Ninja generator add back deleted test.txt, oops gitignore .cache/ Change build file output directory to build/ ... i know, i know... Seth probably intended to clean up the directory... but i need it for clangd to work (and the compile command setting refuses to work. probably issue from my end?)

--- RTConsole/windows/windows_compile.bat
+++ RTConsole/windows/windows_compile.bat
@@ -3,16 +3,15 @@
 REM script to compile proton's RTConsole on Windows using the CMakeLists
 REM you can also use vsc's inbuilt "Build" button. Just click on it!
 
-if exist build\RTConsole.exe del build\RTConsole.exe
-if exist build rd /s /q build
-mkdir build
-cd build
+if exist ..\build rd /s /q ..\build
+mkdir ..\build
+cd ..\build
 
-cmake ..
+REM need to use ninja generator since CMAKE_EXPORT_COMPILE_COMMANDS ON needs it
+cmake -G Ninja ..\windows
 cmake --build . --config Release
-REM -- /j 4
 
 echo Copying binaries to ..\bin directory, run from there!
-xcopy /y Release\RTConsole.exe ..\..\bin
+xcopy /y RTConsole.exe ..\..\bin
 
 cd ..
