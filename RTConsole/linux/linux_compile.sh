0ee5bc8ed0838d82de538309c3017bb7239e6f25
m6o <skidmuo@gmail.com>
2026-04-22 14:49:16 +0300

    Finishing touches to support linux properly.
    The linux_compile.sh script should still work the same as it did before.

--- RTConsole/linux/linux_compile.sh
+++ RTConsole/linux/linux_compile.sh
@@ -1,14 +1,10 @@
-#Muodo's TODO before PR - Implement linux compilations.
+#April 22 2026: I had to make the build directory in the actual workspace root for vsc ~muodo
 
-rm build/RTConsole
-mkdir build
-cd build
-cmake -DDEFINE_RELEASE=ON ..
+rm ../build
+mkdir ../build
+cd ../build
+cmake -DDEFINE_RELEASE=ON ../linux
 make -j 4
 echo Copying binaries to ../bin directory, run from there!
-
-
-#21st of April 2026: updated CMakeLists to put bins to ../bin by themselves
-#cp RTConsole ../../bin
 
 cd ..
